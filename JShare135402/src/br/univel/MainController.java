ac1945dff49844932e33070372e8415d5799d8ae
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-04-13 20:20:01 -0300

    Ajustes iniciais na tela do Cliente.

--- JShare135402/src/br/univel/MainController.java
+++ JShare135402/src/br/univel/MainController.java
@@ -1,5 +1,25 @@
 package br.univel;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+
+public class MainController implements Initializable {
 	
+	@FXML
+	private TextField ipServidor, portaServidor;
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+	@FXML
+	private void download() {
+
+	}
+
 }
