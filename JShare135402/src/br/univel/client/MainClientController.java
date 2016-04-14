419321f9902288b3543b76b66d413ec5d403777a
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-04-14 20:27:41 -0300

    Criada estrutura para as action no FXML (conectar/ desconectar), colocada as properties para adicao de conteudo a tabela, criada as variaveis para utilizacao dos componentes no Controller.

--- JShare135402/src/br/univel/client/MainClientController.java
+++ JShare135402/src/br/univel/client/MainClientController.java
@@ -2,18 +2,33 @@ package br.univel.client;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import br.dagostini.jshare.comum.pojos.Arquivo;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-
 public class MainClientController implements Initializable {
-	
+
 	@FXML
-	private TextField ipServidor, portaServidor;
+	private TextField nomeCliente, ipServidor, portaServidor;
+
+	@FXML
+	private TableView<Arquivo> table;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+	@FXML
+	private void conectar() {
+
+	}
+
+	@FXML
+	private void desconectar() {
 
 	}
 
