3748a87f39cd3d2af3d40118aa397c10fe095886
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-04-20 20:40:40 -0300

    Ajustado metodo conectar para a conexao de clientes.

--- JShare135402/src/br/univel/client/MainClientController.java
+++ JShare135402/src/br/univel/client/MainClientController.java
@@ -1,9 +1,15 @@
 package br.univel.client;
 
 import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
-
 import br.dagostini.jshare.comum.pojos.Arquivo;
+import br.dagostini.jshare.comun.Cliente;
+import br.dagostini.jshare.comun.ClienteFX;
+import br.dagostini.jshare.comun.IServer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
@@ -24,6 +30,24 @@ public class MainClientController implements Initializable {
 
 	@FXML
 	private void conectar() {
+		String nome = nomeCliente.getText().trim();
+		String ip = ipServidor.getText().trim();
+		String porta = portaServidor.getText().trim();
+		int numeroPorta = Integer.parseInt(porta);
+
+		ClienteFX clienteFX = new ClienteFX(new Cliente());
+		clienteFX.setNome(nome);
+
+		Registry registry;
+		try {
+			registry = LocateRegistry.getRegistry(ip, numeroPorta);
+			IServer servico = (IServer) registry.lookup(IServer.NOME_SERVICO);
+			servico.registrarCliente(clienteFX.getCliente());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
