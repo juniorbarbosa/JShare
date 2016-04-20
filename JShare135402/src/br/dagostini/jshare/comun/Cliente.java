934ea7a63a80e81344f141746e26c9ed26437e40
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-04-20 20:14:16 -0300

    Retirada as properties da classe e colocado get e set para serialização.

--- JShare135402/src/br/dagostini/jshare/comun/Cliente.java
+++ JShare135402/src/br/dagostini/jshare/comun/Cliente.java
@@ -2,58 +2,36 @@ package br.dagostini.jshare.comun;
 
 import java.io.Serializable;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
-/**
- * Identificação do cliente.
- * 
- * @author fernandod
- *
- */
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 8998030883019232904L;
 
-	private final StringProperty nome = new SimpleStringProperty();
-	private final StringProperty ip = new SimpleStringProperty();
-	private final IntegerProperty porta = new SimpleIntegerProperty();
+	private String nome;
+	private String ip;
+	private int porta;
 
 	public String getNome() {
-		return nome.get();
-	}
-
-	public void setNome(String value) {
-		nome.set(value);
-	}
-
-	public StringProperty nomeProperty() {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public String getIp() {
-		return ip.get();
-	}
-
-	public void setIp(String value) {
-		ip.set(value);
-	}
-
-	public StringProperty ipProperty() {
 		return ip;
 	}
 
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
 	public int getPorta() {
-		return porta.get();
-	}
-
-	public void setPorta(int value) {
-		porta.set(value);
-	}
-
-	public IntegerProperty portaProperty() {
 		return porta;
 	}
+
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+
 }
