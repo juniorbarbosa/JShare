bd9db7ac899691e053c89bfa8fa1acabf288d74a
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-04-20 20:11:20 -0300

    Criada as properties da classe cliente, com a ligacao com a classe cliente que é Serializable.

--- JShare135402/src/br/dagostini/jshare/comun/ClienteFX.java
+++ JShare135402/src/br/dagostini/jshare/comun/ClienteFX.java
@@ -1,5 +1,68 @@
 package br.dagostini.jshare.comun;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ClienteFX {
+
+	private final StringProperty nome = new SimpleStringProperty();
+	private final StringProperty ip = new SimpleStringProperty();
+	private final IntegerProperty porta = new SimpleIntegerProperty();
+	private final ReadOnlyObjectWrapper<Cliente> cliente = new ReadOnlyObjectWrapper<>();
+
+	public ClienteFX(Cliente cliente) {
+		if (cliente == null) {
+			throw new IllegalArgumentException("O cliente deve ser informado");
+		}
+		this.cliente.set(cliente);
+	}
+
+	public Cliente getCliente() {
+		return cliente.get();
+	}
+
+	public ReadOnlyObjectProperty clienteProperty() {
+		return cliente.getReadOnlyProperty();
+	}
+
+	public String getNome() {
+		return nome.get();
+	}
+
+	public void setNome(String value) {
+		nome.set(value);
+	}
+
+	public StringProperty nomeProperty() {
+		return nome;
+	}
+
+	public String getIp() {
+		return ip.get();
+	}
+
+	public void setIp(String value) {
+		ip.set(value);
+	}
+
+	public StringProperty ipProperty() {
+		return ip;
+	}
+
+	public int getPorta() {
+		return porta.get();
+	}
+
+	public void setPorta(int value) {
+		porta.set(value);
+	}
+
+	public IntegerProperty portaProperty() {
+		return porta;
+	}
 
 }
